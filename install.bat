3fbb758004f7bdbb949ef8a528dc7dcc2248dfb8
Tiago de Jesus Neves <tiagojneves.eng@gmail.com>
2017-06-21 14:43:42 -0300

    Mudanças do script trigger

--- install.bat
+++ install.bat
@@ -1,3 +1,12 @@
-runas /user:Suporte powershell.exe Set-ExecutionPolicy Unrestricted -Force
-powershell.exe Start-Process powershell -Verb runAs .\AutoInstall.ps1
-PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+cd /d %~dp0
+
+REM ECHO %CD%
+REM PAUSE
+
+powershell.exe Set-ExecutionPolicy Unrestricted -Force
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\AutoInstall.ps1'"
+powershell.exe Set-ExecutionPolicy Restricted -Force
+
+REM runas /user:Suporte powershell.exe Set-ExecutionPolicy Unrestricted -Force
+REM powershell.exe Start-Process powershell -Verb runAs .\AutoInstall.ps1
+REM PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
