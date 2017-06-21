89e897c74f70acdcc5b14552ec582e025f7120dd
Tiago de Jesus Neves <tiagojneves.eng@gmail.com>
2017-06-21 15:26:44 -0300

    Remoção de códigos de teste comentados

--- install.bat
+++ install.bat
@@ -1,12 +1,4 @@
 cd /d %~dp0
-
-REM ECHO %CD%
-REM PAUSE
-
 powershell.exe Set-ExecutionPolicy Unrestricted -Force
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\AutoInstall.ps1'"
 powershell.exe Set-ExecutionPolicy Restricted -Force
-
-REM runas /user:Suporte powershell.exe Set-ExecutionPolicy Unrestricted -Force
-REM powershell.exe Start-Process powershell -Verb runAs .\AutoInstall.ps1
-REM PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
